b6f59e57be37915e711047ee20506113909f18b8
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2022-10-03 23:32:33 +0200

    fix: add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/jsonld'],
+  plugins: ['~/plugins/jsonld', '~/plugins/google'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
